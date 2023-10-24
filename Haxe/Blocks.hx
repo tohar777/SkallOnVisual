563f0f60c03c6f2c99dfe44fd10b891e0dce3896
tohar777 <84278827+tohar777@users.noreply.github.com>
2023-10-24 20:42:24 +0300

    Add files via upload

---
+++ Haxe/Blocks.hx
@@ -0,0 +1,13 @@
+import js.Browser;
+import commAddons;
+
+enum Addons {
+    //
+    
+    //
+}
+class Blocks {
+    static function main() {
+        trace("hi");
+    }
+}
---
+++ Haxe/Main.hx
@@ -0,0 +1,23 @@
+import js.Browser;
+import Functions;
+class Main {
+    static function main() {
+        //blocks
+        /*
+        text = TEXT
+        blockinput = INPUT
+        direct = ?? not in set yet!
+        
+        */
+        var print = (function(){
+             var blockJson = '
+             {
+                "color":"blue",
+                "text":"print"
+                "function":${}
+             }
+             ';
+        })
+        //blocks END
+    }
+}
